dc5f860e89018cc8a6c3a15aba7129636fe599a1
tomaszorzano <40808662+tomaszorzano@users.noreply.github.com>
2020-04-05 22:03:07 +0200

    Add files via upload

---
+++ Ejercicio_03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numIngresado;
+            
+
+            Console.WriteLine("Ingrese numero: ");
+           
+            if (int.TryParse(Console.ReadLine(), out numIngresado)) 
+            {
+                int i = 0;
+
+                for (int j = 2; j <= numIngresado; j++)
+                {
+                    for (i = 2; i < j; i++)
+                    {
+                        if ((j % i) == 0)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (i == j)
+                    {
+                        Console.WriteLine("{0} es primo", j);
+                    }
+                }
+            }
+      
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio_06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int anioIngresado;
+
+
+            Console.WriteLine("Ingrese anio: ");
+
+            if (int.TryParse(Console.ReadLine(), out anioIngresado))
+            {
+                int i = 0;
+
+
+
+                for (int j = 4; j <= anioIngresado; j++)
+                {
+                    for (i = 4; i < j; i++)
+                    {
+                        
+                        if (j % 4 != 0)
+                        {
+                            if (j % 100 == 0 || j % 400 != 0)
+                            {
+                                break;
+                            }
+                            
+                        }
+
+                    }
+
+                    if (i == j)
+                    {
+                        Console.WriteLine("{0} es un año bisiesto ", j);
+                    }
+                }
+
+
+
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
